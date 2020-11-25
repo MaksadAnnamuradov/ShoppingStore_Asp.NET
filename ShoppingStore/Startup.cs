14c8f371558f73dea5652f272e0e4c9c6db8f6e9
MaksadAnnamuradov <maksad.annamuradow98@gmail.com>
2020-11-25 11:15:36 -0700

    Created necessary Models

--- ShoppingStore/Startup.cs
+++ ShoppingStore/Startup.cs
@@ -1,13 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using ShoppingStore.Models;
 
 namespace ShoppingStore
 {
@@ -23,7 +26,9 @@ namespace ShoppingStore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddDbContext<StoreDbContext>(options => options.UseNpgsql(convertUrlConnectionString(Configuration["DATABASE_URL"])));
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
+            services.AddScoped<IStoreRepository, EFStoreRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,6 +57,27 @@ namespace ShoppingStore
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            SeedData.EnsurePopulated(app);
+
+        }
+
+        private static string convertUrlConnectionString(string url)
+        {
+            if (url is null)
+                throw new ArgumentNullException("It appears you're missing the DATABASE_URL configuration value...");
+            if (!url.Contains("//"))
+                return url;
+            var uri = new Uri(url);
+            var host = uri.Host;
+            var port = uri.Port;
+            var database = uri.Segments.Last();
+            var parts = uri.AbsoluteUri.Split(':', '/', '@');
+            var user = parts[3];
+            var password = parts[4];
+
+            return $"host={host}; port={port}; database={database}; username={user}; password={password}; SSL Mode=Prefer; Trust Server Certificate=true";
         }
     }
 }
+
