5f9eb78cf2169a9abd603a0b508dd7ddfbe14fbe
MaksadAnnamuradov <maksad.annamuradow98@gmail.com>
2020-11-26 12:26:52 -0700

    Some styling on nav bar and correct paging on each category

--- ShoppingStore/Controllers/HomeController.cs
+++ ShoppingStore/Controllers/HomeController.cs
@@ -26,8 +26,12 @@ namespace ShoppingStore.Controllers
             {
                 CurrentPage = productPage,
                 ItemsPerPage = PageSize,
-                TotalItems = repository.Products.Count()
-            }
+                TotalItems = category == null ?
+                    repository.Products.Count() :
+                    repository.Products.Where(e =>
+                        e.Category == category).Count()
+            },
+            CurrentCategory = category
         });
     }
 }
