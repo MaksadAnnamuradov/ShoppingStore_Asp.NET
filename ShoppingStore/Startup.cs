7620a5843cf3223ad3b3e1b2eca67a7a639d4a8a
MaksadAnnamuradov <maksad.annamuradow98@gmail.com>
2020-11-26 12:07:22 -0700

    Added categories option with urls

--- ShoppingStore/Startup.cs
+++ ShoppingStore/Startup.cs
@@ -52,9 +52,16 @@ namespace ShoppingStore
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints => {
+                endpoints.MapControllerRoute("catpage",
+                    "{category}/Page{productPage:int}",
+                    new { Controller = "Home", action = "Index" });
+                endpoints.MapControllerRoute("page", "Page{productPage:int}",
+                    new { Controller = "Home", action = "Index", productPage = 1 });
+                endpoints.MapControllerRoute("category", "{category}",
+                    new { Controller = "Home", action = "Index", productPage = 1 });
                 endpoints.MapControllerRoute("pagination",
-                "Products/Page{productPage}",
-                new { Controller = "Home", action = "Index" });
+                    "Products/Page{productPage}",
+                    new { Controller = "Home", action = "Index", productPage = 1 });
                 endpoints.MapDefaultControllerRoute();
             });
 
