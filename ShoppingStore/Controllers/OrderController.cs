07437927c56537624919f5b5e6f6fad613e79eeb
MaksadAnnamuradov <maksad.annamuradow98@gmail.com>
2020-11-27 15:37:28 -0700

    Added Order Model and Controller

---
+++ ShoppingStore/Controllers/OrderController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoppingStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingStore.Controllers
+{
+    public class OrderController : Controller
+    {
+        public ViewResult Checkout() => View(new Order());
+    }
+}
