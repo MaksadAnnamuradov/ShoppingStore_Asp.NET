89ad47b0c3e2d40ad91f049d9400331ab6142ece
Maksad Annamuradov <maksad.annamuradow98@gmail.com>
2022-01-10 21:23:25 -0700

    added admin account controller and migrations

---
+++ ShoppingStore/Migrations/AppIdentityDb/20220111035602_IdenttityDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ShoppingStore.Migrations.AppIdentityDb
+{
+    public partial class IdenttityDbContext : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
