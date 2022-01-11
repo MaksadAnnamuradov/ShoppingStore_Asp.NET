182e62337d964c73bc7f5f478280ec18e38160f5
Maksad Annamuradov <maksad.annamuradow98@gmail.com>
2022-01-10 21:24:46 -0700

    bumped to .net5.0 version and some changes in startup

--- ShoppingStore/Startup.cs
+++ ShoppingStore/Startup.cs
@@ -46,15 +46,14 @@ namespace ShoppingStore
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsProduction())
             {
-                app.UseDeveloperExceptionPage();
+                app.UseExceptionHandler("/error");
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                app.UseDeveloperExceptionPage();
+                app.UseStatusCodePages();
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
